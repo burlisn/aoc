f9526f838f03d318d3f31a6a5955d5880ebd974e
Nathan Burlis <burlisn@gmail.com>
2025-12-25 18:44:55 -0600

    day4 part 2

--- 2025/day4/main.cpp
+++ 2025/day4/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdint>
 
 bool IsInGrid(const std::vector<std::vector<char>>& aGrid, const size_t aR, const size_t aC) {
   return (aR < aGrid.size() && aC < aGrid.at(aR).size());
@@ -17,7 +18,7 @@ uint32_t GetAdjacentRolls(const std::vector<std::vector<char>>& aGrid, const siz
       }
       const auto nr = aR + dr;
       const auto nc = aC + dc;
-      if (IsInGrid(aGrid, nr, nc) && aGrid.at(nr).at(nc) == '@') {
+      if (IsInGrid(aGrid, nr, nc) && (aGrid.at(nr).at(nc) == '@' || aGrid.at(nr).at(nc) == 'x')) {
         ++adjacentRolls;
       } 
     }
@@ -25,7 +26,7 @@ uint32_t GetAdjacentRolls(const std::vector<std::vector<char>>& aGrid, const siz
   return adjacentRolls;
 }
 
-uint32_t RemoveRolls(std::vector<std::vector<char>>& aGrid) {
+uint32_t MarkRollsForRemoval(std::vector<std::vector<char>>& aGrid) {
   auto rollsRemoved = 0;
   for (auto r = size_t{}; r < aGrid.size(); ++r) {
     for (auto c = size_t{}; c < aGrid.at(r).size(); ++c) {
@@ -34,12 +35,22 @@ uint32_t RemoveRolls(std::vector<std::vector<char>>& aGrid) {
       }
       const auto adjacentRolls = GetAdjacentRolls(aGrid, r, c);
       if (adjacentRolls < 4) {
-        aGrid.at(r).at(c) = '.';
+        aGrid.at(r).at(c) = 'x';
         ++rollsRemoved;
       }
     }
   }
   return rollsRemoved;
+}
+
+void RemoveRolls(std::vector<std::vector<char>> &aGrid) {
+  for (auto r = size_t{}; r < aGrid.size(); ++r) {
+    for (auto c = size_t{}; c < aGrid.at(r).size(); ++c) {
+      if (aGrid.at(r).at(c) == 'x') {
+        aGrid.at(r).at(c) = '.';
+      }
+    }
+  }
 }
 
 int main() {
@@ -56,23 +67,20 @@ int main() {
     }
   }
 
-  auto removedRoll = false;
+  auto round = 0;
+  auto rolls = 0;
   do {
-
-  } while (removedRoll == true);
-
-  for (auto r = size_t{}; r < grid.size(); ++r) {
-    for (auto c = size_t{}; c < grid.at(r).size(); ++c) {
-      if (grid.at(r).at(c) != '@') {
-        continue;
-      }
-      const auto adjacentRolls = GetAdjacentRolls(grid, r, c);
-      if (adjacentRolls < 4) {
-        ++ans1;
-      }
+    rolls = MarkRollsForRemoval(grid);
+    RemoveRolls(grid);
+    std::cout << "rolls: " << rolls << std::endl;
+    if (round == 0) {
+      ans1 = rolls;
     }
-  }
+    ans2 += rolls;
+    ++round;
+  } while (rolls > 0);
 
   std::cout << "ans1: " << ans1 << std::endl;
+  std::cout << "ans2: " << ans2 << std::endl;
   return 0;
 }
